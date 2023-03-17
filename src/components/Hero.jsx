40660ae7e406334e89e56033e41b590097264435
mohammad rezaii <rezaiimohammad003@gmail.com>
2023-03-17 11:46:30 +0330

    responsive computer model for mobile devices & finishing the Hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -15,14 +15,33 @@ const Hero = () => {
         </div>
 
         <div>
-          <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className="text-[#915eff]">Mohammad</span></h1>
+          <h1 className={`${styles.heroHeadText} text-white`}>
+            Hi, I'm <span className="text-[#915eff]">Mohammad</span>
+          </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I develop 3D visuals, user <br className="hidden sm:block"/> interfaces and web applications
+            I develop 3D visuals, user <br className="hidden sm:block" />{" "}
+            interfaces and web applications
           </p>
         </div>
       </div>
 
       <ComputersCanvas />
+
+      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+        <a href="#about">
+          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
+            <motion.div
+              animate={{ y: [0, 24, 0] }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: "loop",
+              }}
+              className="w-3 h-3 rounded-full bg-secondary mb-1"
+            ></motion.div>
+          </div>
+        </a>
+      </div>
     </section>
   );
 };
